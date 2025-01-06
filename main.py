edef231c315c0ec71c5e1654a5af620da604a239
Cyprien Boscher <cypbo@hotmail.com>
2025-01-06 22:04:23 +0100

    New histogram of number of flights by time of day

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import pandas as pd
 
-import plot_code.histogram_nb_flights_nb_minutes
+import plot_code.histogram_nb_flights_per_delay
+import plot_code.histogram_nb_flights_per_hours
 
 def main():
     df = pd.read_csv("./csv/flights.csv", encoding='latin1')
-    plot_code.histogram_nb_flights_nb_minutes.exec(df)
+    plot_code.histogram_nb_flights_per_delay.exec(df)
+    plot_code.histogram_nb_flights_per_hours.exec(df)
+
 
 if __name__ == "__main__":
     main()
